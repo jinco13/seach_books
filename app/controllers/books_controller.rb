f5e9081e414bde32191bc1c4ce512c05d4971712
jinco13 <jinco13@users.noreply.github.com>
2017-10-12 05:26:25 +0900

    added param check and route for back button

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,11 @@
 class BooksController < ApplicationController
+  
   def search
+    if params[:title]
+      session[:search_title] = params[:title]
+    end
     @searchstr = params[:title]
-    @list = SearchService.search(@searchstr)
+    @list = SearchService.search(session[:search_title])
   end
 
   def index
